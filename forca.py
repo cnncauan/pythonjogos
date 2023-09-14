5200926405c482eedcd23ca420f96ccdd1a0db7f
cnncauan <cnmarcos00gmail.com>
2023-09-14 00:05:55 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,8 @@
+def jogar():
+
+    print("********************************")
+    print("Bem vindos no jogo de forca")
+    print("********************************")
+
+if(__name__ == "__main__"):
+    jogar()
